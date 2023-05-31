6f4b457b3420bc52bc3fdc965e287fadb7ea8b2a
Willem Jouret <willem.jouret44@gmail.com>
2023-06-01 01:06:04 +0200

    display palette feature ;)

--- src/counter.jsx
+++ src/counter.jsx
@@ -31,52 +31,90 @@ export function Counter() {
   const changeColor = () => {
     setColor(randomColor)
   }
-  return (
-    <div className='main-container'>
-      <div className='count-container'>
-        <div
-          className='count'
-          style={count != 0 ? { color: color } : { color: null }}>
-          {count}
-        </div>
 
-        <div
-          style={count != 0 ? { color: color } : { color: null }}
-          className='color'>
-          {count == 0 ? null : color}
+  const closeBtn = () => {
+    return true
+  }
+  return (
+    <>
+      <div className='counter-container'>
+        <div className='number-container'>
+          <div
+            className='count'
+            style={count != 0 ? { color: color } : { color: null }}>
+            {count}
+          </div>
+
+          <div
+            style={count != 0 ? { color: color } : { color: null }}
+            className='color'>
+            {count == 0 ? null : color}
+          </div>
         </div>
-      </div>
-      <div>
-        <span>Choose wich color you wanna pick 😁</span>
-      </div>
-      <div className='choose-btn-container'>
-        {count !== 0 ? (
-          <button className='choose-btn' onClick={handleAddColor}>
-            Choose color
+        <div>
+          <span>Choose wich color you wanna pick 😁</span>
+        </div>
+        <div className='choose-btn-container'>
+          {count !== 0 ? (
+            <button className='btn choose-btn' onClick={handleAddColor}>
+              Choose color
+            </button>
+          ) : null}
+        </div>
+        <div className='iteration-container'>
+          <button
+            className='btn'
+            onClick={() => {
+              handleSubtractOne()
+              changeColor()
+            }}>
+            -1
           </button>
+          <button
+            className='btn'
+            onClick={() => {
+              handleAddOne()
+              changeColor()
+            }}>
+            +1
+          </button>
+        </div>
+        <button className='btn' onClick={resetCounter}>
+          Reset
+        </button>
+      </div>
+
+      <div className='palette-container'>
+        {colors.length ? (
+          <div
+            className='first-color-palette'
+            style={{ backgroundColor: colors[0] }}>
+            <span className='btn close-btn' onClick={closeBtn}>
+              x
+            </span>
+          </div>
+        ) : null}
+        {colors.length > 1 ? (
+          <div
+            className='second-color-palette'
+            style={{ backgroundColor: colors[1] }}>
+            {' '}
+            <span className='btn close-btn' onClick={closeBtn}>
+              x
+            </span>
+          </div>
+        ) : null}
+        {colors.length > 2 ? (
+          <div
+            className='third-color-palette'
+            style={{ backgroundColor: colors[2] }}>
+            {' '}
+            <span className='btn close-btn' onClick={closeBtn}>
+              x
+            </span>
+          </div>
         ) : null}
       </div>
-      <div className='iteration-container'>
-        <button
-          className='btn'
-          onClick={() => {
-            handleSubtractOne()
-            changeColor()
-          }}>
-          -1
-        </button>
-        <button
-          className='btn'
-          onClick={() => {
-            handleAddOne()
-            changeColor()
-          }}>
-          +1
-        </button>
-      </div>
-      <button className='' onClick={resetCounter}>
-        Reset
-      </button>
-    </div>
+    </>
   )
 }
