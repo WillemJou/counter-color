f7786744ab78409f77e6c5e929612f86ad1ba93c
Willem Jouret <willem.jouret9@gmail.com>
2024-04-16 12:46:14 +0200

    Add style to the modal + Close modal with Echap keyboard

--- src/modal.jsx
+++ src/modal.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export function Modal(props) {
   if (!props.isOpen) {
@@ -13,30 +13,52 @@ export function Modal(props) {
     props.setName('')
   }
 
+  useEffect(() => {
+    const keyListener = (e) => {
+      if (e.key === 'Escape') {
+        props.onClose()
+      }
+    }
+    window.addEventListener('keydown', keyListener)
+    return () => window.removeEventListener('keydown', keyListener)
+  }, [])
+
   return (
     <dialog
       open
       aria-modal='true'
       onClick={props.onClose}
       className='fixed h-full w-full flex items-center justify-center bg-neutral-400/75 bottom-0'>
-      <section className='w-2/3 bg-white' onClick={(e) => e.stopPropagation()}>
+      <section
+        className='w-2/3 bg-zinc-100'
+        onClick={(e) => e.stopPropagation()}>
         <header className='relative flex items-end p-2.5 truncat'>
-          <h1 className='title'>Would you name your new palette ?</h1>
+          <h1 className='title'>Name your palette ?</h1>
           <button
             className='border-none cursor-pointer w-[20px] h-auto absolute top-[2px] right-[2px] bg-[rgba(203,_167,_167,_0.902)]'
             onClick={props.onClose}>
             x
           </button>
         </header>
-        <div className='p-2.5 border-y-2'>
+        <div className='m-2.5 border-y-2'>
           <input
             type='text'
             onChange={(e) => props.handleAddPaletteName(e)}
             value={props.name}
             className='w-full'></input>
         </div>
-        <button onClick={() => EventBtn()}>No</button>
-        <button onClick={() => EventBtn()}>Yes</button>
+        <div className='flex justify-center gap-x-3.5 mb-1'>
+          <button
+            className='p-1 border border-slate-400'
+            onClick={() => EventBtn()}>
+            Yes
+          </button>
+          <button
+            className='p-1 border border-slate-400'
+            onClick={() => EventBtn()}>
+            No
+          </button>
+        </div>
       </section>
     </dialog>
   )
